7964e91d5f1cb1de33d13826f07044a512e8f911
dany <1574839461@qq.com>
2018-11-20 15:49:45 +0800

    feat: add test1 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,13 @@ export default function() {
   function click() {
     router.push("/test");
   }
+  function click1() {
+    router.push("/test1");
+  }
   return (
     <div className={styles.normal}>
       <Button onClick={click}>test页面shell部署</Button>
+      <Button onClick={click1}>shell部署</Button>
     </div>
   );
 }
