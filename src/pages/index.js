90835560c43b7a4b18e5434e002b94a651c1c011
dany <1574839461@qq.com>
2018-11-21 10:07:02 +0800

    feat: bug修复

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,8 @@ export default function() {
       <Button onClick={click}>测试31</Button>
       <Button onClick={click1}>策士4</Button>
       <Button onClick={click2}>测试3</Button>
-      <Button onClick={click3}>123请问23423423</Button>
-      <Button onClick={click3}>345请问2342343</Button>
+      <Button onClick={click2}>123请问23423423</Button>
+      <Button onClick={click2}>345请问2342343</Button>
     </div>
   );
 }
